9c43f1a375ab3fa836224fe92f1ee448265caed4
Anowarul Kabir <akabir0101@gmail.com>
2020-07-20 18:23:59 -0400

    new job added

--- job_on_cpu.sh
+++ job_on_cpu.sh
@@ -3,11 +3,11 @@
 ## this must be run from directory where run.py exists.
 ## --workdir is not used in this file.
 
-#SBATCH --job-name=val_batch
+#SBATCH --job-name=test_set_fragments
 #SBATCH --qos=csqos
 ##SBATCH --workdir=/scratch/akabir4/project_dir
-#SBATCH --output=/scratch/akabir4/val_batch/csbw_20_project/outputs/logs/val_batch_log-%N-%j.output
-#SBATCH --error=/scratch/akabir4/val_batch/csbw_20_project/outputs/logs/val_batch_log-%N-%j.error
+#SBATCH --output=/scratch/akabir4/test_set_fragments/csbw_20_project/outputs/logs/test_set_fragments_log-%N-%j.output
+#SBATCH --error=/scratch/akabir4/test_set_fragments/csbw_20_project/outputs/logs/test_set_fragments_log-%N-%j.error
 #SBATCH --mail-user=<akabir4@gmu.edu>
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -17,6 +17,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32000MB
 
-## python full_run_recon_distmap_using_ae.py
+##python full_run_recon_distmap_using_ae.py
 ##python ADMM/test_ADMM.py
-python datasets/data_generator.py
+##python datasets/data_generator.py
+python PyRosetta/fragmet_translation.py
