a11e3fd643f2d4d061b461fae3e2a13746b5e217
Anowarul Kabir <akabir0101@gmail.com>
2020-08-29 20:49:42 -0400

    new Selector named AllBackboneAtomSelector added

--- datasets/my_pdb_data.py
+++ datasets/my_pdb_data.py
@@ -6,44 +6,6 @@ from Bio.PDB.PDBIO import PDBIO, Select
 
 import configs as CONFIGS
 
-class CAAtomSelector(Select):
-    def __init__(self, chain_id):
-        self.chain_id = chain_id
-    
-    def  accept_chain(self, chain):
-        # print(chain.id)
-        if chain.id == self.chain_id:
-            return 1
-        else:
-            return 0
-        
-    def accept_atom(self, atom):
-        """Overload this to reject atoms for output."""
-        if atom.name == "CA":
-            return 1
-        else:
-            return 0
-        
-        
-
-class ChainAndAminoAcidSelect(Select):
-    def __init__(self, chain_id):
-        self.chain_id = chain_id
-        
-    def  accept_chain(self, chain):
-        # print(chain.id)
-        if chain.id == self.chain_id:
-            return 1
-        else:
-            return 0
-            
-    def accept_residue(self, residue):
-        if residue.get_resname() in standard_aa_names:
-            return 1
-        else:
-            return 0
-
-
 class MyPDBData(object):
     """
     Abstract parent class. When new datatype needs to be extracted from PDB data,
@@ -105,3 +67,67 @@ class MyPDBData(object):
         for chain in chains:
             if chain.id == chain_id:
                 return chain
+            
+class CAAtomSelector(Select):
+    def __init__(self, chain_id):
+        self.chain_id = chain_id
+    
+    def  accept_chain(self, chain):
+        # print(chain.id)
+        if chain.id == self.chain_id:
+            return 1
+        else:
+            return 0
+        
+    def accept_atom(self, atom):
+        """Overload this to reject atoms for output."""
+        if atom.name == "CA":
+            return 1
+        else:
+            return 0
+
+class ChainAndAminoAcidSelect(Select):
+    def __init__(self, chain_id):
+        self.chain_id = chain_id
+        
+    def  accept_chain(self, chain):
+        # print(chain.id)
+        if chain.id == self.chain_id:
+            return 1
+        else:
+            return 0
+            
+    def accept_residue(self, residue):
+        if residue.get_resname() in standard_aa_names:
+            return 1
+        else:
+            return 0
+
+class AllBackboneAtomSelector(Select):
+    """Backbone atoms: CA, CB, N, O
+
+    Args:
+        Select ([type]): [description]
+    """
+    def __init__(self, chain_id):
+        self.chain_id = chain_id
+    
+    def  accept_chain(self, chain):
+        # print(chain.id)
+        if chain.id == self.chain_id:
+            return 1
+        else:
+            return 0
+        
+    def accept_atom(self, atom):
+        """Overload this to reject atoms for output."""
+        if atom.name == "CA":
+            return 1
+        elif atom.name == "CB":
+            return 1
+        elif atom.name == "N":
+            return 1
+        elif atom.name == "O":
+            return 1
+        else:
+            return 0
